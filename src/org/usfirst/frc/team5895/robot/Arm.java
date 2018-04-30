b5d1836e35e2209c091c1b87c6732457650118a8
Daniel Cheng <danielcheng0105@gmail.com>
2018-04-30 16:31:50 -0400

    Arm Setup

--- src/org/usfirst/frc/team5895/robot/Arm.java
+++ src/org/usfirst/frc/team5895/robot/Arm.java
@@ -1,5 +1,20 @@
 package org.usfirst.frc.team5895.robot;
 
-public class Arm {
+import edu.wpi.first.wpilibj.Talon;
 
+public class Arm {
+	private Talon armMotor;
+	private double armSpeed;
+	private enum Position {BACK, UP, DOWN};
+	private Position pos = Position.DOWN;
+	int whateverPort;
+	
+	public Arm () {
+		armMotor = new Talon(whateverPort);
+		armSpeed = 0.0;
+	}
+	
+	public void run() {
+		
+	}
 }
